4adb6f9c170707e274054bb280d9e2f2d2712f88
jessieyu <jessieyu@us.ibm.com>
2021-05-07 16:39:51 -0400

    fix linter issues

--- qiskit_runtime/qka/featuremaps.py
+++ qiskit_runtime/qka/featuremaps.py
@@ -1,18 +1,39 @@
-import numpy as np
+# This code is part of qiskit-runtime.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quantum Kernel Alignment feature map."""
+
 import json
+
+import numpy as np
 
 from qiskit import QuantumCircuit, QuantumRegister
 
+
 class FeatureMap:
-    """Mapping data with the feature map.
-    """
+    """Mapping data with the feature map."""
 
     def __init__(self, feature_dimension, entangler_map=None):
-        """
+        """FeatureMap constructor.
+
         Args:
-            feature_dimension (int): number of features (twice the number of qubits for this encoding)
-            entangler_map (list[list]): connectivity of qubits with a list of [source, target], or None for full entanglement.
-                                        Note that the order in the list is the order of applying the two-qubit gate.
+            feature_dimension (int): Number of features (twice the number of
+                qubits for this encoding).
+            entangler_map (list[list]): Connectivity of qubits with a list of [source, target],
+                or None for full entanglement.
+                Note that the order in the list is the order of applying the two-qubit gate.
+
+        Raises:
+            ValueError: If an input value is invalid.
         """
 
         if isinstance(feature_dimension, int):
@@ -26,42 +47,48 @@ class FeatureMap:
         self._num_qubits = int(feature_dimension/2)
 
         if entangler_map is None:
-            self._entangler_map = [[i, j] for i in range(self._num_qubits) for j in range(i + 1, self._num_qubits)]
+            self._entangler_map = [[i, j] for i in range(self._num_qubits) \
+                                   for j in range(i + 1, self._num_qubits)]
         else:
             self._entangler_map = entangler_map
 
         self._num_parameters = self._num_qubits
 
-
     def construct_circuit(self, x=None, parameters=None, q=None, inverse=False, name=None):
         """Construct the feature map circuit.
 
         Args:
-            x (numpy.ndarray): data vector of size feature_dimension
-            parameters (numpy.ndarray): optional parameters in feature map
-            q (QauntumRegister): the QuantumRegister object for the circuit
-            inverse (bool): whether or not to invert the circuit
+            x (numpy.ndarray): Data vector of size feature_dimension.
+            parameters (numpy.ndarray): Optional parameters in feature map.
+            q (QauntumRegister): The QuantumRegister object for the circuit.
+            inverse (bool): Whether or not to invert the circuit.
+            name (str): The name of the quantum circuit. If not set, an
+                automatically generated string will be assigned.
 
         Returns:
-            QuantumCircuit: a quantum circuit transforming data x
+            QuantumCircuit: A quantum circuit transforming data x.
+
+        Raises:
+            ValueError: If an input value is invalid.
         """
 
         if parameters is not None:
-            if isinstance(parameters, int) or isinstance(parameters, float):
+            if isinstance(parameters, (int, float)):
                 raise ValueError('Parameters must be a list.')
-            elif (len(parameters) == 1):
+            if len(parameters) == 1:
                 parameters = parameters * np.ones(self._num_qubits)
             else:
                 if len(parameters) != self._num_parameters:
-                    raise ValueError('The number of feature map parameters must be {}.'.format(self._num_parameters))
+                    raise ValueError(
+                        f"The number of feature map parameters must be {self._num_parameters}.")
 
         if len(x) != self._feature_dimension:
-            raise ValueError('The input vector must be of length {}.'.format(self._feature_dimension))
+            raise ValueError(f"The input vector must be of length {self._feature_dimension}.")
 
         if q is None:
             q = QuantumRegister(self._num_qubits, name='q')
 
-        circuit=QuantumCircuit(q, name=name)
+        circuit = QuantumCircuit(q, name=name)
 
         for i in range(self._num_qubits):
             circuit.ry(-parameters[i], q[i])
@@ -75,8 +102,7 @@ class FeatureMap:
 
         if inverse:
             return circuit.inverse()
-        else:
-            return circuit
+        return circuit
 
     def to_json(self):
         """Return JSON representation of this object.
@@ -96,6 +122,6 @@ class FeatureMap:
             data (str): JSON string representing an object.
 
         Returns:
-            An instance of this class.
+            FeatureMap: An instance of this class.
         """
         return cls(**json.loads(data))
--- qiskit_runtime/qka/kernel_matrix.py
+++ qiskit_runtime/qka/kernel_matrix.py
@@ -1,21 +1,38 @@
-import numpy as np
+# This code is part of qiskit-runtime.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quantum Kernel Alignment kernel matrix."""
+
 import itertools
 
+import numpy as np
+
 from qiskit.compiler import transpile
+
 
 class KernelMatrix:
     """Build the kernel matrix from a quantum feature map."""
 
     def __init__(self, feature_map, backend, initial_layout=None):
-        """
+        """KernelMatrix constructor.
         Args:
-            feature_map (int): the feature map object
-            backend (Backend): the backend instance
-            initial layout (list or dict): initial position of virtual qubits on the physical qubits of the quantum device
+            feature_map (FeatureMap): The feature map object
+            backend (Backend): The backend instance
+            initial layout (list or dict): Initial position of virtual qubits on
+                the physical qubits of the quantum device
         """
 
         self._feature_map = feature_map
-        self._feature_map_circuit = self._feature_map.construct_circuit # the feature map circuit
+        self._feature_map_circuit = self._feature_map.construct_circuit  # the feature map circuit
         self._backend = backend
         self._initial_layout = initial_layout
 
@@ -28,12 +45,14 @@ class KernelMatrix:
         states Phi^dag(y)Phi(x)|0> for input vectors x and y.
 
         Args:
-            x1_vec (numpy.ndarray): NxD array of training data or test data, where N is the number of samples and D is the feature dimension
-            x2_vec (numpy.ndarray): MxD array of training data or support vectors, where M is the number of samples and D is the feature dimension
-            parameters (numpy.ndarray): optional parameters in feature map
+            x1_vec (numpy.ndarray): NxD array of training data or test data, where
+                N is the number of samples and D is the feature dimension.
+            x2_vec (numpy.ndarray): MxD array of training data or support vectors, where
+                M is the number of samples and D is the feature dimension.
+            parameters (numpy.ndarray): Optional parameters in feature map.
 
         Returns:
-           mat (numpy.ndarray): the kernel matrix
+           numpy.ndarray: the kernel matrix
        """
 
         is_identical = False
@@ -42,15 +61,20 @@ class KernelMatrix:
 
         experiments = []
 
-        measurement_basis = '0' * self._feature_map._num_qubits
+        measurement_basis = '0' * self._feature_map._num_qubits  # pylint: disable=protected-access
 
         if is_identical:
 
-            my_product_list = list(itertools.combinations(range(len(x1_vec)), 2)) # all pairwise combos of datapoint indices
+            # all pairwise combos of datapoint indices
+            my_product_list = list(itertools.combinations(range(len(x1_vec)), 2))
             for index_1, index_2 in my_product_list:
 
-                circuit_1 = self._feature_map_circuit(x=x1_vec[index_1], parameters=parameters, name='{}_{}'.format(index_1, index_2))
-                circuit_2 = self._feature_map_circuit(x=x1_vec[index_2], parameters=parameters, inverse=True)
+                circuit_1 = self._feature_map_circuit(x=x1_vec[index_1],
+                                                      parameters=parameters,
+                                                      name='{}_{}'.format(index_1, index_2))
+                circuit_2 = self._feature_map_circuit(x=x1_vec[index_2],
+                                                      parameters=parameters,
+                                                      inverse=True)
                 circuit = circuit_1.compose(circuit_2)
                 circuit.measure_all()
 
@@ -59,24 +83,27 @@ class KernelMatrix:
             program_data = self._run_circuits(experiments)
             self.results['program_data'] = program_data
 
-            mat = np.eye(len(x1_vec), len(x1_vec))  # kernel matrix element on the diagonal is always 1
+            # kernel matrix element on the diagonal is always 1
+            mat = np.eye(len(x1_vec), len(x1_vec))
             for experiment, [index_1, index_2] in enumerate(my_product_list):
 
                 counts = program_data.get_counts(experiment = experiment)
                 shots = sum(counts.values())
 
-                mat[index_1][index_2] = counts.get(measurement_basis, 0) / shots # kernel matrix element is the probability of measuring all 0s
-                mat[index_2][index_1] = mat[index_1][index_2] # kernel matrix is symmetric
-
-            return mat
-
+                # kernel matrix element is the probability of measuring all 0s
+                mat[index_1][index_2] = counts.get(measurement_basis, 0) / shots
+                # kernel matrix is symmetric
+                mat[index_2][index_1] = mat[index_1][index_2]
         else:
-
             for index_1, point_1 in enumerate(x1_vec):
                 for index_2, point_2 in enumerate(x2_vec):
 
-                    circuit_1 = self._feature_map_circuit(x=point_1, parameters=parameters, name='{}_{}'.format(index_1, index_2))
-                    circuit_2 = self._feature_map_circuit(x=point_2, parameters=parameters, inverse=True)
+                    circuit_1 = self._feature_map_circuit(x=point_1,
+                                                          parameters=parameters,
+                                                          name='{}_{}'.format(index_1, index_2))
+                    circuit_2 = self._feature_map_circuit(x=point_2,
+                                                          parameters=parameters,
+                                                          inverse=True)
                     circuit = circuit_1.compose(circuit_2)
                     circuit.measure_all()
 
@@ -96,10 +123,12 @@ class KernelMatrix:
                     mat[index_1][index_2] = counts.get(measurement_basis, 0) / shots
                     i += 1
 
-            return mat
+        return mat
 
     def _run_circuits(self, circuits):
         """Execute the input circuits."""
 
-        transpiled = transpile(circuits, backend=self._backend, initial_layout=self._initial_layout)
+        transpiled = transpile(circuits,
+                               backend=self._backend,
+                               initial_layout=self._initial_layout)
         return self._backend.run(transpiled, shots=8192).result()
