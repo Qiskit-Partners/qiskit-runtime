a6a504d0caaed3375e156b0069165fcbe1f6bc52
delapuente <salva@unoyunodiez.com>
2021-07-28 21:46:31 -0400

    Restore redirecting functionallity to log everything

--- qiskit_runtime/test_server/launcher.py
+++ qiskit_runtime/test_server/launcher.py
@@ -3,6 +3,7 @@
 import json
 import importlib
 from typing import Any, Type
+from contextlib import ExitStack, redirect_stdout, redirect_stderr
 
 from qiskit.providers.ibmq.runtime import UserMessenger
 from qiskit.providers.ibmq.runtime.utils import RuntimeEncoder
@@ -12,32 +13,35 @@ from rq import get_current_job, get_current_connection
 
 class TestServerUserMessenger(UserMessenger):
     """
-    A messenger for sending communications back to the test server through Redis.
+    A messenger for redirecting executions logs to a file and streaming communications
+    back to the test server through Redis.
 
     Must be used within a `with` block, the return value of the `with` block will be the
     messanger itself.
 
-    All messages are sent back to the test sever through Redis, and the final result
-    is also persisted in disk.
+    All messages are sent back to the test sever through Redis, and all standard output,
+    errors and final result are also persistend in disk.
     """
 
-    def __init__(self, print_messages=False):
+    def __init__(self):
         job = get_current_job()
-        self._print_messages = print_messages
         self._log_file = job.meta["log_path"]
         self._result_path = job.meta["result_path"]
         self._channel_id = job.meta["channel_id"]
         self._connection = get_current_connection()
+        self._exit_stack = ExitStack()
         self._context_started = False
 
     def __enter__(self):
         self._log_file = open(self._log_file, "w")
-        self._log_file.__enter__()
+        self._exit_stack.enter_context(self._log_file)
+        self._exit_stack.enter_context(redirect_stdout(self._log_file))
+        self._exit_stack.enter_context(redirect_stderr(self._log_file))
         self._context_started = True
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        return self._log_file.__exit__(exc_type, exc_val, exc_tb)
+        return self._exit_stack.__exit__(exc_type, exc_val, exc_tb)
 
     def publish(
         self, message: Any, encoder: Type[json.JSONEncoder] = RuntimeEncoder, final: bool = False
@@ -47,10 +51,10 @@ class TestServerUserMessenger(UserMessenger):
 
         text_message = json.dumps(message, cls=encoder)
 
+        self._publish_on_stdout(text_message, final)
         self._publish_on_redis(text_message, final)
-        self._publish_on_disk(text_message, final)
-        if self._print_messages:
-            self._publish_on_stdout(text_message, final)
+        if final:
+            self._publish_result_on_disk(text_message)
 
     def _publish_on_stdout(self, message: str, _: bool) -> None:
         """Print the message on stdout."""
@@ -63,21 +67,16 @@ class TestServerUserMessenger(UserMessenger):
         redis_payload = redis_message.encode("utf-8")
         self._connection.publish(self._channel_id, redis_payload)
 
-    def _publish_on_disk(self, message: str, final: bool) -> None:
+    def _publish_result_on_disk(self, message: str) -> None:
         """Log the message on log and result files."""
-
-        if not final:
-            self._log_file.write(message)
-
-        else:
-            with open(self._result_path, "w") as result_file:
-                result_file.write(message)
+        with open(self._result_path, "w") as result_file:
+            result_file.write(message)
 
 
 def launch(program_module_path, simulator_name, kwargs):
     """Prepare and launch the Qiskit Runtime program."""
 
-    with TestServerUserMessenger(print_messages=False) as user_messenger:
+    with TestServerUserMessenger() as user_messenger:
         from qiskit import Aer
 
         program_module = importlib.import_module(program_module_path)
